9e5150174b94fb76628565a3063c17ce6ce6d427
Alin Cijov <rcijov>
2018-08-18 12:12:43 -0400

    Added the perceptron graph

--- perceptron.py
+++ perceptron.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 np.random.seed(42)
 
-
 def stepFunction(t):
     if t >= 0:
         return 1
@@ -16,30 +15,19 @@ def stepFunction(t):
 def prediction(X, W, b):
     return stepFunction((np.matmul(X,W)+b)[0])
 
-# TODO: Fill in the code below to implement the perceptron trick.
-# The function should receive as inputs the data X, the labels y,
-# the weights W (as an array), and the bias b,
-# update the weights and bias W, b, according to the perceptron algorithm,
-# and return W and b.
 def perceptronStep(X, y, W, b, learn_rate = 0.01):
-    # Fill in code
     for i in range(len(X)):
-        y_hat = prediction(X[i], W, b)
-        if y[i]-y_hat == 1:
-            W[0] = W[0]+learn_rate*X[i][0]
-            W[1] = W[1]+learn_rate*X[i][1]
-            b = b+learn_rate
-        if y[i]-y_hat == -1:
-            W[0] = W[0]-learn_rate*X[i][0]
-            W[1] = W[1]-learn_rate*X[i][1]
-            b = b-learn_rate
+        pred = prediction(X[i],W,b)
+        if y[i]-pred == 1:
+            W[0] += X[i][0]*learn_rate
+            W[1] += X[i][1]*learn_rate
+            b += learn_rate
+        elif y[i]-pred == -1:
+            W[0] -= X[i][0]*learn_rate
+            W[1] -= X[i][1]*learn_rate
+            b -= learn_rate
     return W, b
 
-# This function runs the perceptron algorithm repeatedly on the dataset,
-# and returns a few of the boundary lines obtained in the iterations,
-# for plotting purposes.
-# Feel free to play with the learning rate and the num_epochs,
-# and see your results plotted below.
 def trainPerceptronAlgorithm(X, y, learn_rate = 0.01, num_epochs = 25):
     x_min, x_max = min(X.T[0]), max(X.T[0])
     y_min, y_max = min(X.T[1]), max(X.T[1])
@@ -51,17 +39,20 @@ def trainPerceptronAlgorithm(X, y, learn_rate = 0.01, num_epochs = 25):
         # In each epoch, we apply the perceptron step.
         W, b = perceptronStep(X, y, W, b, learn_rate)
         boundary_lines.append((-W[0]/W[1], -b/W[1]))
-    return boundary_lines
+    return W,b
 
 if __name__ == "__main__":
     import pandas as pd
     columns=['x1','x2','y']
     df=pd.read_csv("data.csv", header=None, names=columns)
     X=np.array(df.values[:,:2])
-    y=np.array(df.values[:,2:-1])
-    print(X.shape)
-    print(y.shape)
-    trainPerceptronAlgorithm(X, y)
+    y=np.array(df.values[:,2:])
+    result = trainPerceptronAlgorithm(X,y)
+    yzero = - result[1] / result[0][0]
+    xzero = - result[1] / result[0][1]
+    xx = [0,yzero]
+    yy = [xzero,0]
+    plt.plot(xx,yy)
     xes = X[:,:1].reshape(-1)
     yes = X[:,1:].reshape(-1)
     plt.plot(xes[:50],yes[:50], 'ro')
